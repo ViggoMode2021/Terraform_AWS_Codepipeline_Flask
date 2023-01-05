c4873f7a8bd078e69e9d9fb95a6823145833feb1
ryanv <ryansviglione@gmail.com>
2023-01-05 17:22:26 -0500

    hey

--- index.py
+++ index.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/services/service-2")
-def hello():
-    return "This is a sample response from service 2 (Python App Service) v2"
-
-
-@app.route("/services/service-2/status")
-def status():
-    return "ok"
-
+    time = datetime.datetime.now()
+    return render_template("index.html", time=time)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=False)
