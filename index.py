908ae0c002a755083d906ea9cdef5ca609ca7dbc
ryansviglione@gmail.com <ryansviglione@gmail.com>
2023-01-05 01:06:41 +0000

    Edited index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ok I just designed a new space car zooooooooooooted"
+    return "ok I just designed a new space car zooooooooooooted check it out yall"
 
 @app.route("/services/service-2")
 def hello():
