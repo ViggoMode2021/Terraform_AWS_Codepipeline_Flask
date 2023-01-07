b7490d9ce4f3dd4bcb6b8d672c4d591645424c2a
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-01-06 23:01:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    time = datetime.datetime.now()
-    return render_template("index.html", time=time)
+    return "chyeah"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=False)
